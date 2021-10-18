7373c283d5cd7ea37548882163f5c87fcc883ab0
ZhukovMikhail <79285451+ZhukovMikhail@users.noreply.github.com>
2021-10-19 00:22:56 +0300

    done
    done

--- src/App.js
+++ src/App.js
@@ -1,63 +1,38 @@
 import './App.css';
-import React, { Component } from 'react';
+import { useState } from 'react';
+// import React, { Component } from 'react';
 import Statistics from './Components/Statistics/Statistics';
 import FeedbackOptions from './Components/FeedbackOptions/FeedbackOptions.jsx';
 
-class App extends Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
-  };
-  compValue = Object.keys(this.state)[0];
-  data = {
-    compValue: 0,
-    total: 0,
-    percentageOfcompValue: 0,
-  };
+const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
 
-  hendleFeedback = e => {
-    const { name } = e.currentTarget;
-    console.log(name);
-    this.setState(prev => ({ [name]: prev[name] + 1 }));
-    if (name === this.compValue) {
-      this.data.compValue += 1;
-    }
-    this.hendleTotalValue();
-    this.onpercentageOfcompValue();
-  };
+  const total = good + neutral + bad;
+  const percentageOfgood = Math.floor((good / total) * 100);
 
-  hendleTotalValue = e => (this.data.total += 1);
-
-  onpercentageOfcompValue = e => {
-    this.data.percentageOfcompValue = Math.floor(
-      (this.data.compValue / this.data.total) * 100,
-    );
-    console.log('good', [Object.keys(this.state)[0]]);
-    console.log('total', this.data.total);
-    console.log('percentageOfcompValue', this.data.percentageOfcompValue);
-  };
-
-  render() {
-    return (
-      <div className="section">
-        <h2 className="title">Please leave your feedback</h2>
-        <div className="btn">
-          <FeedbackOptions
-            options={['good', 'neutral', 'bad']}
-            onLeaveFeedback={this.hendleFeedback}
-          />
-        </div>
-        <Statistics
-          good={this.state.good}
-          neutral={this.state.neutral}
-          bad={this.state.bad}
-          total={this.data.total}
-          positivePercentage={this.data.percentageOfcompValue}
+  return (
+    <div className="section">
+      <h2 className="title">Please leave your feedback</h2>
+      <div className="btn">
+        <FeedbackOptions
+          options={[
+            ['good', () => setGood(good + 1)],
+            ['neutral', () => setNeutral(neutral + 1)],
+            ['bad', () => setBad(bad + 1)],
+          ]}
         />
       </div>
-    );
-  }
-}
+      <Statistics
+        good={good}
+        neutral={neutral}
+        bad={bad}
+        total={total}
+        positivePercentage={percentageOfgood}
+      />
+    </div>
+  );
+};
 
 export default App;
--- src/Components/FeedbackOptions/FeedbackOptions.jsx
+++ src/Components/FeedbackOptions/FeedbackOptions.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 import styles from './FeedbackOptions.module.css';
 import PropTypes from 'prop-types';
 
-const FeedbackOptions = ({ options, onLeaveFeedback }) => {
+const FeedbackOptions = ({ options }) => {
   return options.map(option => (
     <button
       type="button"
       className={styles.button}
-      key={option}
-      name={option}
-      onClick={onLeaveFeedback}
+      key={option[0]}
+      name={option[0]}
+      onClick={option[1]}
     >
       {option}
     </button>
@@ -19,7 +19,7 @@ const FeedbackOptions = ({ options, onLeaveFeedback }) => {
 export default FeedbackOptions;
 
 FeedbackOptions.propTypes = {
-  options: PropTypes.string,
+  options: PropTypes.array,
   onLeaveFeedback: PropTypes.func,
   button: PropTypes.string,
 };
